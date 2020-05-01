201511f7b2f8c7e92d7428aaf8eb95e31918bd2d
alex22barreto <alexyuls503@gmail.com>
2020-04-30 21:58:06 -0500

    proyectos como targetas

--- src/main/webapp/app/vista-proyectos/targeta-proyectos/targeta-proyectos.component.ts
+++ src/main/webapp/app/vista-proyectos/targeta-proyectos/targeta-proyectos.component.ts
@@ -1,12 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+import { JhiEventManager } from 'ng-jhipster';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { IProyecto } from 'app/shared/model/proyecto.model';
+import { ProyectoSTargetaService } from './targeta-proyectos.service';
 
 @Component({
   selector: 'jhi-targeta-proyectos',
   templateUrl: './targeta-proyectos.component.html',
   styleUrls: ['./targeta-proyectos.component.scss']
 })
-export class TargetaProyectosComponent implements OnInit {
-  constructor() {}
+export class TargetaProyectosComponent implements OnInit, OnDestroy {
+  proyectos?: IProyecto[];
+  eventSubscriber?: Subscription;
 
-  ngOnInit(): void {}
+  constructor(
+    protected proyectoService: ProyectoSTargetaService,
+    protected eventManager: JhiEventManager,
+    protected modalService: NgbModal
+  ) {}
+
+  loadAll(): void {
+    this.proyectoService.query().subscribe((res: HttpResponse<IProyecto[]>) => (this.proyectos = res.body || []));
+  }
+
+  ngOnInit(): void {
+    this.loadAll();
+    this.registerChangeInProyectos();
+  }
+
+  ngOnDestroy(): void {
+    if (this.eventSubscriber) {
+      this.eventManager.destroy(this.eventSubscriber);
+    }
+  }
+
+  trackId(index: number, item: IProyecto): number {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+    return item.id!;
+  }
+
+  registerChangeInProyectos(): void {
+    this.eventSubscriber = this.eventManager.subscribe('proyectoListModification', () => this.loadAll());
+  }
 }
